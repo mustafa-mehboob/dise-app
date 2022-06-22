6a366074564e4253908a001f4d5f8a0c3afcf833
ahmedraza41190 <57095619+mustafa-mehboob@users.noreply.github.com>
2022-06-22 17:06:39 +0500

    update

---
+++ app.js
@@ -0,0 +1,11 @@
+function myfunction() {
+    var nam = Math.random() * 6;
+    
+    var result = Math.round(nam)
+    
+    document.getElementById("dise").innerHTML = result;
+    console.log(result);
+}
+
+
+
